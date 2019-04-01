25fad1f1e0ee2f271048ccf8bcbae008a50d03d0
Karol Marczyk <KarMarcz@gmail.com>
2019-04-01 21:02:34 +0200

    Configuration Adnotation

--- src/main/java/com/example/JokeApp/services/ChuckNorrisImpl.java
+++ src/main/java/com/example/JokeApp/services/ChuckNorrisImpl.java
@@ -1,6 +1,7 @@
 package com.example.JokeApp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,9 +9,13 @@ public class ChuckNorrisImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public ChuckNorrisImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public ChuckNorrisImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
+
+    //    public ChuckNorrisImpl() {
+//        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+//    }
 
     @Override
     public String getJoke() {
